221235fa6c67b8afe1a2865e84d4b9f3c5f7bef3
gitwell <thinkweiwei@msn.com>
2018-11-18 00:14:53 +0800

    函数A

---
+++ 02_write_a_program/code.py
@@ -0,0 +1,18 @@
+import platform
+
+'''
+打印python版本
+'''
+# print(platform.python_version())
+
+'''
+从键盘读取字符串
+'''
+# name = input('What is your first name? ').strip()  # 使用函数strip()将开头和末尾的空白字符删除
+# print('Hello ' + name.capitalize() + '!')  # 函数name.capitalize()确保字符串的第一个字符为大写，而其他字符为小写
+
+'''
+在print语句中，必须将整数变量转换为字符串，这样才能打印它
+'''
+# print("I'm " + str(10) + ' years old')
+
